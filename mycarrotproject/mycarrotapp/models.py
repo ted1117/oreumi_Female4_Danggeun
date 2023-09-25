b64a31dbbbe8612f3de5a637bd671b8b03c76738
Koo Youngjae <skcyclwm0313@naver.com>
2023-09-25 17:56:19 +0900

    Feat models, views, urls, html

--- mycarrotproject/mycarrotapp/models.py
+++ mycarrotproject/mycarrotapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class DanggeunPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=200)
+    price = models.IntegerField()
+    status = models.CharField(max_length=1, default='N')
+    views = models.IntegerField(default=0)
+    chat = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    author_id = models.CharField(max_length=100, null=True, blank=True)
+    post_id = models.CharField(max_length=100, null=True, blank=True)
+    trading_location = models.CharField(max_length=100)
+    publish = models.CharField(max_length=1, default='Y')
--- mycarrotproject/mycarrotapp/urls.py
+++ mycarrotproject/mycarrotapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.main, name="main"),
+    path('trade_list/', views.trade_list, name='trade_list'),
+    path('trade_post/', views.trade_post, name='trade_post'),
+    path('write/', views.create_or_update_post, name='create_or_update_post'),
 ]
