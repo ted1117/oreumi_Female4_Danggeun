2a11d50064be89f911c3f2a2c628c8f9d73dd18e
Koo Youngjae <skcyclwm0313@naver.com>
2023-09-26 17:35:09 +0900

    Feat: 게시물 수정 및 추가

--- mycarrotproject/mycarrotapp/models.py
+++ mycarrotproject/mycarrotapp/models.py
@@ -1,17 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
-class DanggeunPost(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=200)
     price = models.IntegerField()
-    status = models.CharField(max_length=1, default='N')
-    views = models.IntegerField(default=0)
-    chat = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    author_id = models.CharField(max_length=100, null=True, blank=True)
-    post_id = models.CharField(max_length=100, null=True, blank=True)
-    trading_location = models.CharField(max_length=100)
-    publish = models.CharField(max_length=1, default='Y')
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='post_images/') 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, to_field='username')
+    created_at = models.DateTimeField(auto_now_add=True, null=True) 
+
+    product_reserved = models.CharField(max_length=1, default='N')  # 예약 여부
+    product_sold = models.CharField(max_length=1, default='N')  # 판매 여부
+
+    view_num = models.PositiveIntegerField(default=0)  # 조회 수
+    chat_num = models.PositiveIntegerField(default=0)  # 채팅 수
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at']
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    region = models.CharField(max_length=100, null=True)
+    region_certification = models.CharField(max_length=1, default='N')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
