8528445d44df1ae75071321cb5d9512b70377d9c
moon <moonj96@gmail.com>
2023-09-26 09:10:15 +0900

    Add < 최문정/ login, register, main - html, css>

--- mycarrotproject/mycarrotapp/urls.py
+++ mycarrotproject/mycarrotapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'mycarrotapp'
+
 urlpatterns = [
     path("", views.main, name="main"),
     path("chat/", views.chat, name="chat"),
     path("chat/<str:room_name>/", views.room, name="room"),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
--- mycarrotproject/mycarrotapp/views.py
+++ mycarrotproject/mycarrotapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .forms import CustomLoginForm, CustomRegistrationForm
 
 # Create your views here.
+
+# 메인페이지
 def main(request):
     return render(request, "main.html")
 
@@ -12,3 +15,13 @@ def room(request, room_name):
         "room_name": room_name,
     }
     return render(request, "chat/room.html", context)
+
+# 로그인
+def login(request):
+    form = CustomLoginForm(data=request.POST or None)
+    return render(request,'login.html',{'form':form})
+
+# 회원가입
+def register(request):
+    form = CustomRegistrationForm(request.POST)
+    return render(request, 'register.html', {'form':form})
