67d6fd3c7fe6a48875d6fc70f1f42656ce0e7204
moon <moonj96@gmail.com>
2023-09-26 17:31:50 +0900

    Add <최문정 / models.py>

--- mycarrotproject/mycarrotapp/models.py
+++ mycarrotproject/mycarrotapp/models.py
@@ -15,3 +15,13 @@ class DanggeunPost(models.Model):
     post_id = models.CharField(max_length=100, null=True, blank=True)
     trading_location = models.CharField(max_length=100)
     publish = models.CharField(max_length=1, default='Y')
+
+class UserInfo(models.Model):
+    user_name = models.CharField(max_length=20) # 이름
+    nickname = models.CharField(max_length=100) # 닉네임
+    region = models.CharField() # 지역
+    manner_temp = models.IntegerField() # 매너온도
+    region_cert = models.CharField(max_length=1, default='N') # 지역인증 여부
+    create_date =models.DateTimeField(auto_now_add=True) # 가입일
+    # account_id = models.CharField() # 회원 계정 ID
+    # password = models.CharField() # 비밀번호
