7787385b6280844219ade8787cfb3a55ef1c28f9
ted111712 <ted111712@gmail.com>
2023-10-03 22:52:02 +0900

    Feat: 인공지능 챗봇 기능 추가

--- mycarrotproject/mycarrotapp/consumers.py
+++ mycarrotproject/mycarrotapp/consumers.py
@@ -1,12 +1,15 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Chat
+import openai
+from django.conf import settings
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
+        openai.api_key = settings.OPENAI_KEY
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -26,8 +29,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json["message"]
         username = text_data_json["username"]  # username을 추출
         time = text_data_json["time"]
+        isGPT = text_data_json["isGPT"]
 
-        # await self.create_chat_message(message, is_read=True)
+        await self.create_chat(message, is_read=True)
+
+        if isGPT:
+            message = self.get_GPT_response(message)
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -39,6 +46,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "time": time,
             },
         )
+
+    def get_GPT_response(self, user_message):
+        # OpenAI GPT-3.5-turbo로 응답 생성
+        response = openai.Completion.create(
+            engine="text-davinci-003",  # GPT-3.5-turbo를 사용하려면 "text-davinci-003"으로 설정
+            prompt=user_message,
+            max_tokens=50,
+            temperature=0.7
+        )
+        gpt3_turbo_response = response.choices[0].text.strip()
+        return gpt3_turbo_response
 
     # stackoverflow
     from channels.db import database_sync_to_async
@@ -52,7 +70,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event["message"]
         username = event["username"]  # username을 추출
         time = event["time"]
-        new_msg = await self.create_chat(message, is_read=True)
+        #isGPT = event["isGPT"]
+        #new_msg = await self.create_chat(message, is_read=True)
 
         # Send message and username to WebSocket
         await self.send(
